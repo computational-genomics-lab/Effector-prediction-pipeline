4679e6d48c5f4dfa32d222efc6f9b12b1ab180e9
AditiMaulik <130646439+AditiMaulik@users.noreply.github.com>
2024-04-18 18:15:54 +0530

    Add files via upload

--- tmhmm_result_processing.py
+++ tmhmm_result_processing.py
@@ -3,25 +3,68 @@ import pandas as pd
 import sys
 import csv
 from  pyfaidx import Fasta
+'''
+#command line a kotogulo argumnets pass kora holo
+n=len(sys.argv)
+#print("total no of arguments passed :", n)
 
-# Get a list of files ending with '_summary.fa' in the current working directory
-arr_txt = [x for x in os.listdir() if x.endswith('_summary.fa')]
+#print("\nName of the python script", sys.argv[0])
 
-# Iterate through each file in the list
-for i in arr_txt:
-    # Open the Fasta file
-    genes = Fasta(i)
+#search for corresponding protein file with ending of _translated.fna
+#arr_txt = [x for x in os.listdir() if x.endswith("_translated.fna")]
 
-# Create an empty list to store sequences
-new_fasta = []
+data=[]
+data1=[]
 
-# Open the CSV file
+#take the basename without extension .signalp5
+basename= (os.path.splitext(sys.argv[1])[0])
+
+#1st argument e je signalp file ta deoa holo seta open kore read kora holo; then ID, Prediction coloumn duto dia dictionary create kora holo jekhane ID key ar Prediction value
+with open(sys.argv[1],"r") as f:
+	#next(f)
+	signalp_file=pd.read_csv(f,header=None)
+	print(signalp_file.head())
+	
+	ID=(signalp_file.loc[:,0])
+	Prediction=(signalp_file.loc[:,4])
+	#print(ID)
+	mydict=dict(zip(ID,Prediction))
+	for ID,Prediction in mydict.items():
+		if Prediction == 'PredHel=0':
+			lab=[]
+			lab=[ID]
+			data.append(lab)
+#making the output csv name as the input file name without extension
+outputFileName= basename + ".txt"
+with open(outputFileName,'w') as f:
+	writer=csv.writer(f)
+	
+	#write the data
+	writer.writerows(data)
+'''
+#current work directory te file gulo jader end '_translated.fna' dia end sei file gulo neoa; singnalp file r corresponding protein file theke
+#signal peptide sequence gulo retrieve kora
+
+arr_txt= [ x for x in os.listdir() if x.endswith('_summary.fa')]
+
+for i in arr_txt :
+	#b=(os.path.splitext(i)[0])
+	#print(b)
+	#b1=b + "_tmhmm_result"
+	#print(b1)
+	#if (b1==basename):
+        #print(i)
+        genes=Fasta(i)
+
+new_fasta=[]
 with open("tmp3") as csv_file:
-    # Iterate through each row in the CSV file
-    for row in csv_file:
-        # Retrieve the sequence corresponding to each entry in the CSV file from the Fasta file
-        new_fasta.append(">%s\n%s" % (row.strip("\n"), (genes[row.strip("\n")])))
-
-# Write the sequences to an output Fasta file
-with open("output.fa", 'w') as f:
-    f.write('\n'.join(new_fasta))
+	#csv_reader=csv.reader(csv_file,delimiter=',')
+	for row in csv_file:
+		#print(row)
+		#print(">%s\n%s" % (row.strip("\n"),(genes[row.strip("\n")])))
+		new_fasta.append(">%s\n%s" % (row.strip("\n"),(genes[row.strip("\n")])))
+#output=basename + '.fa'
+with open("output.fa",'w') as f:
+	f.write('\n'.join(new_fasta))
+        
+        
